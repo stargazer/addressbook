133b2ecb79009a8558165313086bc7c1bd6bfed6
stargazer <already.late@gmail.com>
2020-08-25 11:28:09 +0300

    Defines placeholder for controller functions

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+const Contact = require('../models/contact.model.js');
+
+exports.create = (req, res) => {
+
+};
+
+exports.findAll = (req, res) => {
+
+};
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.updateOne = (req, res) => {
+
+};
+
+exports.deleteOne = (req, res) => {
+
+};
