ef6f0a69253338ce4a60d97f03ed99cfc783f49a
stargazer <already.late@gmail.com>
2020-08-25 10:44:07 +0300

    Set up and configure the database

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Configure db
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+// Connect to db
+mongoose.connect(dbConfig.url, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Successfully connected to the database.");
+}).catch(err => {
+  console.log("Could not connect to the database. ", err);
+  process.exit();
+});
+
 app.get('/', (req, res) => {
   res.json({"message": "Hello world"})
 });
