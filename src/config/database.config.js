6ddab6aa3ed6dd7676f060165ea1421f75a8e280
stargazer <already.late@gmail.com>
2020-08-26 13:51:54 +0300

    Refactor DB connection

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  url: 'mongodb://db:27017/addressbook'
-}
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+url = 'mongodb://db:27017/addressbook';
+
+// Connect to db and export connection
+module.exports = mongoose.connect(url, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Successfully connected to the database.");
+}).catch(err => {
+  console.log("Could not connect to the database. ", err);
+  process.exit();
+});
--- src/index.js
+++ src/index.js
@@ -6,23 +6,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Configure db
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-// Connect to db
-mongoose.connect(dbConfig.url, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log("Successfully connected to the database.");
-}).catch(err => {
-  console.log("Could not connect to the database. ", err);
-  process.exit();
-});
+// Initiate DB connection
+require('./config/database.config.js');
 
 // Contacts routes
-require('./app/routes/contact.routes.js')(app);
+require('./api/routes/contact.routes.js')(app);
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
