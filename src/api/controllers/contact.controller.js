64918ff2ffc0148024a745b19b690273cf334c55
stargazer <already.late@gmail.com>
2020-09-25 10:53:23 +0300

    Rewrites controllers
    Controller functions are now written using async/await, rather than
    using explitic promise handling.

--- src/api/controllers/contact.controller.js
+++ src/api/controllers/contact.controller.js
@@ -1,15 +1,15 @@
 const Contact = require('../models/contact.model.js');
 
-exports.create = (req, res) => {
+async function create(req, res) {
   // Validate
   if(!req.body.name && !req.body.surname) {
-    res.status(400).send({
+    return res.status(400).send({
       message: "Contact needs to have at least a name or surname"
     });
   }
 
   // Create Contact object
-  const contact = new Contact({
+  const contact = await new Contact({
     name: req.body.name,
     surname: req.body.surname,
     phones: req.body.phones,
@@ -17,33 +17,32 @@ exports.create = (req, res) => {
   });
 
   // Save Contact
-  contact.save()
-   .then(data => {
-     res.send(data);
-   }).catch(err => {
-     res.status(500).send({
-       message: err.message
-     })
-   });
+  try {
+    data = await contact.save();
+    res.send(data);
+  } catch(err) {
+    res.status(500).send({
+      message: err.message
+    });
+  };
+}
+
+async function findAll(req, res) {
+  try {
+    contacts = await Contact.find();
+    res.send(contacts);
+  } catch(err) {
+    res.status(500).send({
+      message: err.message
+    });
+  }
 };
 
-exports.findAll = (req, res) => {
-  Contact.find()
-   .then(contacts => {
-     res.send(contacts);
-   }).catch(err => {
-     res.status(500).send({
-       message: err.message
-     });
-   });
-
-};
-
-exports.findOne = (req, res) => {
-  Contact.findById(req.params.contactId)
-   .then(contact => {
-     res.send(contact);
-   }).catch(err => {
+async function findOne(req, res) {
+  try {
+    contact = await Contact.findById(req.params.contactId);
+    res.send(contact);
+  } catch(err) {
     if(err.kind === 'ObjectId') {
       res.status(404).send({
         message: "Contact with id " + req.params.contactId + " not found"
@@ -52,48 +51,56 @@ exports.findOne = (req, res) => {
     res.status(500).send({
       message: err.message
     });
-   });
+  }
 };
 
-exports.updateOne = (req, res) => {
+async function updateOne(req, res) {
   // Validate
   if(!req.body.phones && !req.body.emails) {
-    res.status(400).send({
+    return res.status(400).send({
       message: "You need to provide some data to update the contact"
     });
   }
 
   // Update Contact object
-  Contact.findByIdAndUpdate(req.params.contactId, {
-    phones: req.body.phones || [],
-    emails: req.body.emails || []
-  }, {new: true})
-   .then(contact => {
-     res.send(contact);
-   }).catch(err => {
-     if(err.kind === 'ObjectId') {
+  try {
+    contact = await Contact.findByIdAndUpdate(req.params.contactId, {
+      phones: req.body.phones || [],
+      emails: req.body.emails || []
+    }, {new: true});
+    res.send(contact);
+  } catch(err) {
+    if(err.kind === 'ObjectId') {
       res.status(404).send({
         message: "Contact with id " + req.params.contactId + " not found"
       });
-     }
+    }
     res.status(500).send({
       message: err.message
     });
-   });
-};
+  }
+}
 
-exports.deleteOne = (req, res) => {
-  Contact.findByIdAndRemove(req.params.contactId)
-   .then(contact => {
-     res.send({message: "Contact deleted"});
-   }).catch(err => {
-     if(err.kind === 'ObjectId') {
+async function deleteOne(req, res) {
+  try {
+    contact = await Contact.findByIdAndRemove(req.params.contactId);
+    res.send({message: "Contact deleted"});
+  } catch(err) {
+    if(err.kind === 'ObjectId') {
       res.status(404).send({
         message: "Contact with id " + req.params.contactId + " not found"
       });
-     }
+    }
     res.status(500).send({
       message: err.message
     });
-   });
-};
+  }
+}
+
+module.exports = {
+  create,
+  findAll,
+  findOne,
+  updateOne,
+  deleteOne,
+}
