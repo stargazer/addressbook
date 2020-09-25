640010753af90efc925da4bbb4025fe867daa75e
stargazer <already.late@gmail.com>
2020-09-25 10:20:15 +0300

    Refactor db connection
    Makes use of an async function, to utilize promises in a cleaner manner.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,11 +1,13 @@
 mongoose = require('mongoose');
 dbUrl = 'mongodb://db:27017/addressbook';
 
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log("Successfully connected to the database.");
-}).catch(err => {
-  console.log("Could not connect to the database. ", err);
-  process.exit();
-});
+async function dbConnect(uri) {
+  try {
+    await mongoose.connect(uri, { userNewUrlParser: true });
+    console.log("Successfully connected to the database.");
+  } catch(err) {
+    console.log("Could not connect to the database. ", err);
+    process.exit();
+  }
+}
+dbConnect(dbUrl);
